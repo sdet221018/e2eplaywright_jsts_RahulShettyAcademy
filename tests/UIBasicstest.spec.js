80c4b18b2430f403901fa81333f78d737d8cc8ce
kareem <kshaik_sr2021@yahoo.com>
2024-08-31 22:54:54 +0100

    Test Annotation, async await understanding

---
+++ tests/UIBasicstest.spec.js
@@ -0,0 +1,10 @@
+const {test} = require('@playwright/test')
+
+test('First Playwrigt Test', async ()=> {
+// playwright code is Asynchronous
+// Step 1 -- Open Browser
+// Step 2 -- Enter User Name & Password
+// Step 3 -- Click
+// In JavaScript, all steps are asynchronous and hence they all try to run simultaneously. Hence, the need await keyword.
+
+});
